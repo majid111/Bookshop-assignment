ee7125cd0053040ef440a63702c837f62fc864f9
majid111 <abdullahalmajid81@gmail.com>
2025-02-23 07:06:10 +0600

    server initial added

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+// import { StudentRoutes } from './app/Modules/Student/Student.router';
+const app = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+//application routes
+// app.use('/api/v1/students', StudentRoutes); //students routes
+
+//routes
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!!!!!!!!!!!!!!");
+});
+
+export default app;
